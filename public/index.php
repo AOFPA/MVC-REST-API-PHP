23567266fb650896599e1878483d2feaf5938d53
AOFPA <chalongchaith@gmail.com>
2023-02-28 15:33:52 +0700

    add : comment route

--- public/index.php
+++ public/index.php
@@ -4,24 +4,27 @@ require_once realpath(__DIR__ . "/vendor/autoload.php");
 $router = new Router();
 
 //end point
+//read all user
 $router->map( 'GET', '/api/v1/user', function() {
 	require __DIR__ . '/api/user/get_all.php';
 });
 
+//create a new user
 $router->map( 'POST', '/api/v1/user/create', function() {
 	require __DIR__ . '/api/user/create.php';
 });
 
+//modify user
 $router->map( 'POST', '/api/v1/user/update', function() {
 	require __DIR__ . '/api/user/update.php';
 });
 
-
+//delete user
 $router->map( 'POST', '/api/v1/user/delete', function() {
 	require __DIR__ . '/api/user/delete.php';
 });
 
-
+//read one user
 $router->map( 'GET', '/api/v1/user/[i:id]', function( $id ) {
 	require __DIR__ . '/api/user/get_by_id.php';
 });
